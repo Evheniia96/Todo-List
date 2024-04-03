9ce11d0163971dbf8208da53f3d98ea01f6ff522
Evheniia Ilchenko <ilchenko.evgeniya96@gmail.com>
2024-04-03 13:10:57 +0300

    update urls

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from todo_list.views import IndexView
+from todo_list.views import IndexView, TagsListView
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("tags", TagsListView.as_view(), name="tags_list"),
     ]
 
 app_name = "todo_list"
