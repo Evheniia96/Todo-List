284733cae61b967979fa7cefc0506373413254cf
Evheniia Ilchenko <ilchenko.evgeniya96@gmail.com>
2024-04-03 13:10:37 +0300

    create TagsListView in the views.py

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.views import generic
 
-from todo_list.models import Task
+from todo_list.models import Task, Tag
 
 
 class IndexView(generic.ListView):
     queryset = Task.objects.prefetch_related("tags")
     template_name = "todo_list/index.html"
+
+
+class TagsListView(generic.ListView):
+    model = Tag
+    template_name = "todo_list/tags_list.html"
