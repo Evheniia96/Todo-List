f198cdf880991b8016c713fc55d5f62d65fefdd5
Evheniia Ilchenko <ilchenko.evgeniya96@gmail.com>
2024-04-03 15:59:05 +0300

    create tags_list.html and update urls.py & views.py

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,13 +1,22 @@
 from django.urls import path
 
-from todo_list.views import IndexView, TagsListView, TaskCreateView, TaskUpdateView, TaskDeleteView
+from todo_list.views import IndexView, TagsListView, TaskCreateView, TaskUpdateView, TaskDeleteView, TagTaskListView, \
+    TagDeleteView, TagUpdateView, TagCreateView
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("tags", TagsListView.as_view(), name="tags_list"),
     path("task/create/", TaskCreateView.as_view(), name="task_create"),
     path("task/<int:pk>/update/", TaskUpdateView.as_view(), name="task_update"),
-    path("task/<int:pk>/delate/", TaskDeleteView.as_view(), name="task_delete"),
+    path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="task_delete"),
+    path(
+        "tasks/<int:pk>/add/",
+        TagTaskListView.as_view(),
+        name="task-update-tag"
+    ),
+    path("tag/create/", TagCreateView.as_view(), name="tag_create"),
+    path("tag/<int:pk>/update/", TagUpdateView.as_view(), name="tag_update"),
+    path("tag/<int:pk>/delete/", TagDeleteView.as_view(), name="tag_delete"),
 
     ]
 
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy, reverse
 from django.views import generic
 
 from todo_list.models import Task, Tag
@@ -9,11 +10,9 @@ class IndexView(generic.ListView):
     # queryset = Task.objects.prefetch_related("tags")
     model = Task
     template_name = "todo_list/index.html"
-    context_object_name = "task_list"
 
 
 class TaskCreateView(generic.CreateView):
-
     model = Task
     fields = "__all__"
     success_url = reverse_lazy("todo_list:index")
@@ -31,8 +30,41 @@ class TaskDeleteView(generic.DeleteView):
     model = Task
     success_url = reverse_lazy("todo_list:index")
 
+
 class TagsListView(generic.ListView):
     model = Tag
     template_name = "todo_list/tags_list.html"
 
+
+class TagTaskListView(generic.ListView):
+
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("todo_list:index")
+
+    def get(self, request, *args, **kwargs):
+        task = get_object_or_404(Task, pk=self.kwargs["pk"])
+        task.is_done = not task.is_done
+        task.save()
+        return HttpResponseRedirect(
+            reverse("todo_list:index"))
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo_list:index")
+
+
+class TagUpdateView(generic.UpdateView):
+
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo_list:index")
+
+
+class TagDeleteView(generic.DeleteView):
+
+    model = Tag
+    success_url = reverse_lazy("todo_list:index")
 
